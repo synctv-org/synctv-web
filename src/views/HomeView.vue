84d3e9239fae5d8d03f45c74b78e180ab28c6124
zijiren233 <pyh1670605849@gmail.com>
2023-10-10 23:12:16 +0800

    Opt: delete comment

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,7 +8,6 @@ import RoomList from "@/components/RoomList.vue";
     <br />
     <RoomList />
     <br />
-    <!-- <p class="text-zinc-400">*开发中页面，可能是最终品质</p> -->
     <p>
       &copy; Copyright 2023 <a href="https://github.com/synctv-org" target="_blank">SyncTV</a> all
       right reserved
