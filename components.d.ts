bb0924faa85826cb18177e8b0ec374cc0e2cd29d
Lazy <2211717435@qq.com>
2023-11-16 20:58:38 +0800

    Update components.d.ts

--- components.d.ts
+++ components.d.ts
@@ -22,7 +22,6 @@ declare module 'vue' {
     ElEmpty: typeof import('element-plus/es')['ElEmpty']
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
-    ElIcon: typeof import('element-plus/es')['ElIcon']
     ElInput: typeof import('element-plus/es')['ElInput']
     ElMain: typeof import('element-plus/es')['ElMain']
     ElOption: typeof import('element-plus/es')['ElOption']
