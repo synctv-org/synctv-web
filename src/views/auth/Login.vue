acca75b131b6a452c781b0f1fd566679b19ab1b7
Lazy <2211717435@qq.com>
2023-11-23 15:48:45 +0800

    Feat: default login

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
 import { ElNotification, ElMessage } from "element-plus";
-import { OAuth2Platforms, loginWithOAuth2 } from "@/services/apis/auth";
+import { OAuth2Platforms, loginWithOAuth2, LoginApi } from "@/services/apis/auth";
+import { userInfo } from "@/services/apis/user";
 import { useRouteQuery } from "@vueuse/router";
+import { strLengthLimit } from "@/utils/utils";
+import { userStore } from "@/stores/user";
+import router from "@/router/index";
 
 const platforms: { [key: string]: { name: string; class: string } } = {
   github: {
@@ -18,9 +22,74 @@ const platforms: { [key: string]: { name: string; class: string } } = {
     class: "btn-warning"
   },
   feishuSSO: {
-       name: "飞书SSO",
-       class: "btn-default"
-     }
+    name: "飞书SSO",
+    class: "btn-default"
+  }
+};
+
+const formData = ref({
+  username: localStorage.getItem("uname") || "",
+  password: localStorage.getItem("password") || ""
+});
+const savePwd = ref(false);
+
+const redirect = useRouteQuery("redirect");
+console.log("redirect: ", (redirect.value as string) ?? "");
+
+const { getUserInfo: updateUserInfo, updateToken } = userStore();
+const { execute: reqLoginApi, state: loginData } = LoginApi();
+const login = async () => {
+  if (formData.value?.username === "" || formData.value?.password === "") {
+    ElNotification({
+      title: "错误",
+      message: "请填写表单完整",
+      type: "error"
+    });
+    return;
+  }
+  try {
+    for (const key in formData.value) {
+      strLengthLimit(key, 32);
+    }
+    await reqLoginApi({
+      data: formData.value
+    });
+    if (!loginData.value)
+      return ElNotification({
+        title: "错误",
+        message: "服务器并未返回token",
+        type: "error"
+      });
+
+    updateToken(loginData.value.token);
+    localStorage.setItem("uname", formData.value.username);
+    localStorage.setItem("password", savePwd.value ? formData.value.password : "")
+    
+
+    const state = await userInfo().execute({
+      headers: {
+        Authorization: loginData.value?.token ?? ""
+      }
+    })
+
+    if (state.value) {
+      updateUserInfo(state.value);
+      localStorage.setItem("uname", state.value.username);
+      ElNotification({
+        title: "登录成功",
+        type: "success"
+      });
+
+      router.replace(redirect.value as string || "/");
+    }
+  } catch (err: any) {
+    console.error(err);
+    ElNotification({
+      title: "错误",
+      message: err.response.data.error || err.message,
+      type: "error"
+    });
+  }
 };
 
 const { execute: reqOAuth2PlatformsApi, state: OAuth2Platforms_ } = OAuth2Platforms();
@@ -38,10 +107,6 @@ const getOAuth2Platforms = async () => {
 };
 
 const { state, execute } = loginWithOAuth2();
-const redirect = useRouteQuery("redirect");
-
-console.log("redirect: ", (redirect.value as string) ?? "");
-
 const useOAuth2 = async (platform: string) => {
   try {
     await execute({
@@ -68,14 +133,33 @@ onMounted(async () => {
 
 <template>
   <div class="room">
-    <div class="login-box w-full">
-      <h1 class="text-[28px] font-bold">请选择登陆方式</h1>
+    <form @submit.prevent="" class="login-box">
+      <input
+        class="l-input"
+        type="text"
+        v-model="formData.username"
+        placeholder="用户名"
+        required
+      />
+      <br />
+      <input class="l-input" type="password" v-model="formData.password" placeholder="密码" required />
+      <br />
+      <div class="text-sm"><b>注意：</b>所有输入框最大只可输入32个字符</div>
+      <div>
+        <input class="w-auto" type="checkbox" v-model="savePwd" />
+        <label title="明文保存到本机哦~">&nbsp;记住密码</label>
+      </div>
+      <button class="btn m-[10px]" @click="login">登录</button>
+    </form>
+    <br />
+    <div class="sm:w-96 w-full m-auto">
+      <h4 class="text-[18px] font-bold">使用第三方平台登录</h4>
       <button
         v-for="item in OAuth2Platforms_?.enabled"
         :class="`btn ${platforms[item] ? platforms[item].class : ''} m-[10px]`"
         @click="useOAuth2(item)"
       >
-        使用 {{ platforms[item] ? platforms[item].name : item }} 账号登录
+        {{ platforms[item] ? platforms[item].name : item }}
       </button>
     </div>
   </div>
@@ -87,10 +171,19 @@ onMounted(async () => {
   margin-top: 5vmax;
 
   .login-box {
-    // width: 443px;
-    margin: auto;
+    @apply sm:w-96 w-full m-auto;
+
+    input {
+      width: 70%;
+
+      &:hover {
+        padding: 10px 15px;
+        width: 74%;
+      }
+    }
 
     .btn {
+      width: 70%;
       padding: 10px 15px;
 
       &:hover {
