45fb21377dda6a3c82b60c116ae2fc281925b155
Lazy <2211717435@qq.com>
2023-12-25 13:05:27 +0800

    Opt: alist dialog

--- src/components/fileList/alist.vue
+++ src/components/fileList/alist.vue
@@ -25,16 +25,24 @@ const pageSize = ref(10);
 const dir = ref("");
 const getFileList = async (paths?: string) => {
   if (paths) dir.value = paths;
-  await execute({
-    headers: { Authorization: userToken.value },
-    data: {
-      path: dir.value
-    },
-    params: {
-      page: currentPage.value,
-      max: pageSize.value
-    }
-  });
+  try {
+    await execute({
+      headers: { Authorization: userToken.value },
+      data: {
+        path: dir.value
+      },
+      params: {
+        page: currentPage.value,
+        max: pageSize.value
+      }
+    });
+  } catch (err: any) {
+    return ElNotification({
+      type: "error",
+      title: "错误",
+      message: err.message
+    });
+  }
 };
 
 const { execute: reqPushMoviesApi, isLoading: pushMovieLoading } = pushMoviesApi();
@@ -86,7 +94,13 @@ defineExpose({
 </script>
 <template>
   <el-dialog v-model="open" title="文件列表" class="rounded-lg dark:bg-zinc-800 max-sm:w-full">
-    <index ref="FileList" :fileList="state" @to-dir="getFileList" :is-loading="isLoading" />
+    <index
+      ref="FileList"
+      :fileList="state"
+      @to-dir="getFileList"
+      :is-loading="isLoading"
+      render-type="alist"
+    />
     <template #footer>
       <div class="flex justify-between items-center flex-wrap gap-3 -mt-7">
         <el-pagination
