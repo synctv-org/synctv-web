03bc0dd0cdea9cc69034f0ae79372d629eacee8e
Lazy <2211717435@qq.com>
2023-12-26 09:24:31 +0800

    Opt: select color

--- src/components/fileList/index.vue
+++ src/components/fileList/index.vue
@@ -77,7 +77,7 @@ onMounted(() => {
       class="flex items-center p-2 bg-slate-50 my-2 rounded-md cursor-pointer transition-all duration-300 hover:bg-slate-100 hover:shadow-md hover:scale-[1.02] shadow-slate-300 dark:bg-neutral-700 dark:hover:bg-neutral-600"
       :class="
         findItem(item) &&
-        ' bg-slate-200 hover:shadow-none hover:bg-slate-300 hover:scale-100 dark:bg-neutral-900 dark:hover:bg-stone-800'
+        ' bg-gray-200 hover:shadow-none hover:bg-slate-300 hover:scale-100 dark:bg-neutral-900 dark:hover:bg-stone-800'
       "
       v-for="(item, i) in fileList?.items"
       :key="i"
