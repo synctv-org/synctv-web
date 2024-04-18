74ab3058badb54c5c88dda3afb5b88df8f1444f6
Lazy <2211717435@qq.com>
2024-04-18 16:50:44 +0800

    Feat: add disable_guest setting

--- src/hooks/useSettings.ts
+++ src/hooks/useSettings.ts
@@ -103,6 +103,7 @@ export const useSettings = () => {
   const defaultCinemaSettings: Map<string, settingType> = new Map([
     ["hidden", { value: false, name: "隐藏此房间" }],
     ["disable_join_new_user", { value: false, name: "禁止新用户加入" }],
+    ["disable_guest", { value: false, name: "禁止访客加入" }],
     ["join_need_review", { value: false, name: "加入需要审核" }],
     // ["user_default_permissions", { value: 0, name: "用户默认权限" }],
     ["can_get_movie_list", { value: false, name: "允许用户获取影片列表" }],
