9ed55aa84afb0a7e53c27549b9c3431594b60612
zijiren233 <pyh1670605849@gmail.com>
2024-10-13 11:16:35 +0800

    fix: auto orientation on mobile

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -229,7 +229,7 @@ const newPlayerOption = (html: HTMLDivElement): Option => {
     lock: true, // 移动端显示锁定按钮
     fastForward: true, // 移动端添加长按视频快进功能
     autoPlayback: false, // 使用自动回放功能
-    autoOrientation: false, // 移动端的网页全屏时，根据视频尺寸和视口尺寸，旋转播放器
+    autoOrientation: true, // 移动端的网页全屏时，根据视频尺寸和视口尺寸，旋转播放器
     airplay: false, // 隔空播放
     type: Props.options.type,
     plugins: Props.options.plugins,
